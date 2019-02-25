145d6630eae80dc5dbfb31c46b1a7e574429d792
Daehyeon Kang <naspy001@gmail.com>
2019-02-25 22:18:33 +0900

    day08

--- stack/edu_토너먼트카드게임.py
+++ stack/edu_토너먼트카드게임.py
@@ -1,18 +1,34 @@
 import sys
 sys.stdin = open("토너먼트.txt","r")
 
-def div(data):
-    if len(data) == 1:
-        data[:l]
-        data[l:]
-        [temp1, temp2]
-        div(temp1)
-        div(temp2)
+def winner(a,b):
+    if a[0][1] == b[0][1]:
+        return a
+    if a[0][1] == 3 or b[0][1] == 3:
+        if a[0][1] == 1:
+            return a
+        if b[0][1] == 1:
+            return b
+
+    if a[0][1] > b[0][1]:
+        return a
+    else:
+        return b
+
+def half(data):
+    length = len(data)
+    if length == 1:
+        return data
+    if length == 2:
+        return winner(data[:1],data[1:])
+
+    return winner(half(data[:length//2]),half(data[length//2:]))
+
 
 for tc in range(int(input())):
     n=int(input())
-    data = list(map(int,input().split()))
-    div(data)
+    data = list( (i+1,j) for i,j in enumerate(map(int,input().split())))
+    print(f"#{tc+1} {half(data)[0][0]}")
 
 
 
