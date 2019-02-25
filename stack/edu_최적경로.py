63d8bbf7e354e17a5da33526356d39a56be6bc8f
Daehyun Kang <naspy001@gmail.com>
2019-02-25 19:13:49 +0900

    ìminpath

--- stack/edu_최적경로.py
+++ stack/edu_최적경로.py
@@ -1,46 +1,47 @@
 import sys
 sys.stdin = open("최적경로.txt","r")
 
-def btk(y,nowsum):
+def btk(start,nowsum):
     global minsum, visited
+    road.append(start)
 
-    if y>=n:
-        nowsum += weight[y+2][1]
+    if len(road) == 1:
+        nowsum += weight[0][start+1]
+
+    if len(road)>=n:
+        nowsum += weight[start+1][1]
         if nowsum<minsum:
             minsum = nowsum
-        nowsum -= weight[y + 2][1]
+
+        nowsum -= weight[start+1][1]
         return
 
     if nowsum > minsum:
         return
 
-    for x in range(n):
-        if not visited[x]:
-            nowsum += weight[y+2][x+2]
-            visited[x] = 1
-            btk(y+1,nowsum)
-            nowsum -= weight[y+2][x+2]
-            visited[x] = 0
-
-
-
+    for nxt in range(1,n+1):
+        if weight[start+1][nxt+1] and not visited[nxt]:
+            nowsum += weight[start+1][nxt+1]
+            visited[start] = 1
+            btk(nxt,nowsum)
+            nowsum -= weight[start + 1][nxt + 1]
+            road.pop()
+            visited[nxt] = 0
 
 for tc in range(int(input())):
     n = int(input())
     data = list(map(int,input().split()))
     weight = [[0]*(n+2) for _ in range(n+2)]
     minsum=987654321
-    visited=[0]*n
-    nowsum=0
+
+
 
     for start in range(n+2):
         for end in range(n+2):
             weight[start][end] = abs(data[2*start]-data[2*end]) + abs(data[2*start+1]-data[2*end+1])
-
-    for i in range(n):
-        nowsum+=weight[0][i+2]
-        visited[i]=1
-        btk(i,nowsum)
-        visited[i]=0
-        nowsum-=weight[0][i+2]
+    for start in range(1,n+1):
+        road=[]
+        visited = [0] * (n + 1)
+        btk(start,0)
+    print(f"#{tc+1} {minsum}")
 
