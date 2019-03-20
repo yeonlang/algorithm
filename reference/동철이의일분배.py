33458780d8a21733ab4acc7867eb1888e83cd1e0
Daehyun Kang <naspy001@gmail.com>
2019-03-21 04:09:37 +0900

    code

---
+++ reference/동철이의일분배.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open("동철이의일분배.txt")
+
+for tc in range(int(input())):
+    N = int(input())
+    M = 1 << N
+    dp = [[0.0 for _ in range(M)] for _ in range(N)]
+
+    G = []
+    for i in range(N):
+        G.append(list(map(float, input().split())))
+        for j in range(N):
+            G[i][j] = G[i][j] / 100
+
+    for i in range(N):
+        dp[0][1 << i] = G[0][i]
+
+    for i in range(1, N):
+        for cur in range(1, M):
+            if dp[i - 1][cur] == 0:
+                continue
+
+            for j in range(N):
+                if cur & (1 << j) != 0 or G[i][j] == 0:
+                    continue
+                nxt = cur | (1 << j)
+
+                dp[i][nxt] = max(dp[i][nxt], dp[i - 1][cur] * G[i][j])
+
+    print("#%d %.6f" % (tc+1, dp[N - 1][M - 1] * 100))
