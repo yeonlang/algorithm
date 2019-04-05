93c6bd015cf52a69efe346d84b74783fb025005b
Daehyeon Kang <naspy001@gmail.com>
2019-04-06 01:46:28 +0900

    swtest

--- 테스트/test3.py
+++ 테스트/test3.py
@@ -1,10 +1,10 @@
 def DFS(c,idx):
-    print(result)
+    if result:
+        print(result)
     for i in range(idx,N):
-        if i+1 not in result:
-            result.append(i+1)
-            DFS(c+1,i+1)
-            result.pop()
+        result.append(i+1)
+        DFS(c+1,i+1)
+        result.pop()
 
 N,K = 4,4
 result = []
